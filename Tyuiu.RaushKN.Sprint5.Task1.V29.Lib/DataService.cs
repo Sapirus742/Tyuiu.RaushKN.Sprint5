776a7063bed42010a3c984dc7188e80fe76045be
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-10-07 23:08:07 +0500

    Sprint5

---
+++ Tyuiu.RaushKN.Sprint5.Task1.V29.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RaushKN.Sprint5.Task1.V29.Lib
+{
+    public class DataService : ISprint5Task1V29
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask1.txt" });
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists) File.Delete(path);
+
+            double y;
+            string strY;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = Math.Round((Math.Sin(x) / (x + 1.2)) + Math.Cos(x) * 7 * x - 2,2);
+                strY = Convert.ToString(y);
+                if (x + 1.2 == 0) y = 0;
+
+                if (x != stopValue) File.AppendAllText(path, strY + Environment.NewLine);
+                else File.AppendAllText(path, strY);
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.RaushKN.Sprint5.Task1.V29/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.RaushKN.Sprint5.Task1.V29.Lib;
+
+namespace Tyuiu.RaushKN.Sprint5.Task1.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Рауш К. Н. | АСОиУб-24-1";
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# Спринт #5                                                               #");
+            Console.WriteLine("# Тема: Класс File. Запись набора данных в текстовый файл                 #");
+            Console.WriteLine("# Задание #1                                                              #");
+            Console.WriteLine("# Вариант #29                                                             #");
+            Console.WriteLine("# Выполнил Рауш Кирилл Николавевич | АСОиУб-24-1                          #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# УСЛОВИЕ:                                                                #");
+            Console.WriteLine("# Дана функция, произвести табулирование f(x) на диапазоне [-5,5] с шагом #");
+            Console.WriteLine("# 1. Произвести проверку деления на ноль. При делении на ноль вернуть     #");
+            Console.WriteLine("# значение 0. Результат сохранить в текстовый фойл OutPutFileTask1.txt и  #");
+            Console.WriteLine("# вывести на консоль в таблицу. Значения округлить до двух знаков после   #");
+            Console.WriteLine("# запятой                                                                 #");
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
+            Console.WriteLine("###########################################################################");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Начальное значение диапазона = " + startValue);
+            Console.WriteLine("Конечное значение диапазона = " + stopValue);
+
+            Console.WriteLine("###########################################################################");
+            Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+            Console.WriteLine("###########################################################################");
+        
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
