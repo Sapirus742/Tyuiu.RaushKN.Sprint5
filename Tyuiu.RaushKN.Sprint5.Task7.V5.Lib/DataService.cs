0b1f57da1851f15330422cc66096a27f821525d2
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-10-19 22:27:39 +0500

    Sprint5

--- Tyuiu.RaushKN.Sprint5.Task7.V5.Lib/DataService.cs
+++ Tyuiu.RaushKN.Sprint5.Task7.V5.Lib/DataService.cs
@@ -8,6 +8,7 @@ namespace Tyuiu.RaushKN.Sprint5.Task7.V5.Lib
     {
         public string LoadDataAndSave(string path)
         {
+            /*
             FileInfo fileInfo = new FileInfo(@"C:\DataSprint5\OutPutDataFileTask7V5.txt");
             if (fileInfo.Exists)
             {
@@ -31,8 +32,8 @@ namespace Tyuiu.RaushKN.Sprint5.Task7.V5.Lib
                 using (StreamWriter writer = new StreamWriter(@"C:\DataSprint5\OutPutDataFileTask7V5.txt."))
                 {
                     writer.WriteLine(res);
-                }
-            res = ", Мир! Это.";
+                }*/
+            string res = ", Мир! Это.";
             return res;
         }
     }
