647c5f1946cf01b44408389ecbd88863d8b484e7
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-10-19 19:26:26 +0500

    Sprint5

---
+++ Tyuiu.RaushKN.Sprint5.Task6.V2.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.RaushKN.Sprint5.Task6.V2.Lib
+{
+    public class DataService : ISprint5Task6V2
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.ToUpper();
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char c = line[i];
+                        if ((c >= 'А') && (c <= 'Я'))
+                            count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.RaushKN.Sprint5.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+using Tyuiu.RaushKN.Sprint5.Task6.V2.Lib;
+
+namespace Tyuiu.RaushKN.Sprint5.Task6.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V2.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExist);
+        }
+
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V2.txt";
+            var res = ds.LoadFromDataFile(path);
+            int wait = 9;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
