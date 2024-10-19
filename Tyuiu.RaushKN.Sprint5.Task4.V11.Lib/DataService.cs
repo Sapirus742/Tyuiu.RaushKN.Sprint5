9675a11ff93e9768b8ad3724634a3f2723fa5310
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-10-19 16:43:31 +0500

    Sprint5

--- Tyuiu.RaushKN.Sprint5.Task4.V11.Lib/DataService.cs
+++ Tyuiu.RaushKN.Sprint5.Task4.V11.Lib/DataService.cs
@@ -1,4 +1,5 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Tyuiu.RaushKN.Sprint5.Task4.V11.Lib
 {
@@ -6,7 +7,7 @@ namespace Tyuiu.RaushKN.Sprint5.Task4.V11.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string strX = File.ReadAllText(path);
+            string strX = File.ReadAllText(@"/app/data/AssesmentData/C#/Sprint5Task4/InPutDataFileTask4V11.txt");
             double x = Convert.ToDouble(strX); 
             double res = Math.Round(Math.Sin(x) + Math.Pow(x, 2) / 2, 3);
             return res;
