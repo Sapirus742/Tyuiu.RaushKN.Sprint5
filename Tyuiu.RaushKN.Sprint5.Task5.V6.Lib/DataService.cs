924dc2a85fbd7ce1ef00608efd844279f3c8b1f5
Кирилл Рауш <Tyuiu.RaushKN@outlook.com>
2024-10-21 21:40:26 +0500

    Sprint5

--- Tyuiu.RaushKN.Sprint5.Task5.V6.Lib/DataService.cs
+++ Tyuiu.RaushKN.Sprint5.Task5.V6.Lib/DataService.cs
@@ -8,16 +8,17 @@ public class DataService : ISprint5Task5V6
     {
         double res = 0;
         double count = 0;
-        string[] array = new string[20];
+        string[] array;
         using (StreamReader reader = new StreamReader(path))
         {
             string line;
             while ((line = reader.ReadLine()) != null)
             {
                 line = line.Replace(".", ",");
-                for(int i = 0; i < line.Length; i++)
+                array = line.Split(" ");
+                for(int i = 0; i < array.Length; i++)
                 {
-                    res = res + Math.Round(Convert.ToDouble(line), 3);
+                    res = res + Math.Round(Convert.ToDouble(array[i]), 3);
                     count++;
                 }
                 
